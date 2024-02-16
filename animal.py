120635ea4cf394d003b08b1763db8080a8b0ba10
tishanW98 <shakilatishan1998@outlook.com>
2024-02-16 15:15:55 +0530

    Dog function added

--- animal.py
+++ animal.py
@@ -1,1 +1,17 @@
-this ia a animal file.
+import sys
+
+def dog():
+    print('Baw')
+
+def default():
+    print('Hello')
+
+def main():
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
+
