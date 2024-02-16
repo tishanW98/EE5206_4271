c69ef28899cdc286ed25aa0dc8fdb3bf9bf3ee41
tishanW98 <shakilatishan1998@outlook.com>
2024-02-16 14:58:01 +0530

    Cat function added

--- animal.py
+++ animal.py
@@ -1,1 +1,17 @@
-this ia a animal file.
+import sys
+
+def cat():
+    print('Meow')
+
+def default():
+    print('Hello')
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
+
